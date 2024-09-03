a67a7239d7fd886524d817ff042b85a2bee30867
unknown <vishakvijayan2002@gmail.com>
2024-09-03 19:43:37 +0530

    sunRise v1

---
+++ main.js
@@ -0,0 +1,32 @@
+let listBg= document.querySelectorAll('.banner .bg');
+let titleBanner=document.querySelector('.banner h1');
+let listTab=document.querySelectorAll('.tab');
+
+window.addEventListener('scroll',(event)=>{
+    // scrollY i the web scrollbar position (pixel)
+    let top = this.scrollY;
+    // 
+    listBg.forEach((bg, index)=>{
+        if(index != 0 && index != 8)
+        {
+            bg.style.transform=`translateY(${top*index/2}px)`;
+        }
+        else if(index==0)
+        {
+            bg.style.transform=`translateY(${top/3}px)`; 
+        }
+    })
+
+    titleBanner.style.transform = `translateY(${top*4/2}px)`;
+
+    listTab.forEach(tab=>{
+        if(tab.offsetTop - top < 550)
+        {
+            tab.classList.add('active');
+        }
+        else
+        {
+            tab.classList.remove('active')
+        }
+    })
+})
